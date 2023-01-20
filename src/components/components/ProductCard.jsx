09f26a0dee3b88c14045580832b27dffbcba686c
sandeep-gsis <sandeep.gsis@gmail.com>
2023-01-20 13:39:36 +0400

    Modifies Price Section in Card in mobile screens

--- src/components/components/ProductCard.jsx
+++ src/components/components/ProductCard.jsx
@@ -5,7 +5,7 @@ const ProductCard = ({ product, discount }) => {
         <div className='card bg-white rounded-2xl md:rounded-xl overflow-hidden px-3 py-3 flex flex-col gap-2 h-full '>
             <img src={product.image} alt='Phone' />
             <p className='text-[.6875rem] md:text-[.875rem]'>{product.title}</p>
-            <div className='inline-flex gap-3 items-center '>
+            <div className='inline-flex flex-col md:flex-row gap-1 md:gap-3 md:items-center '>
                 <p className='font-bold'>AED {product.price}</p>
                 <p className='line-through text-gray-400 j text-[.625rem] md:text-[.75rem]'>
                     AED {product.prevPrice}
