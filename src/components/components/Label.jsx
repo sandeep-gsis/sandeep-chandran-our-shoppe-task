e45adaf4febfecafc606ed5506732d38e357f2f9
sandeep-gsis <sandeep.gsis@gmail.com>
2023-01-20 13:34:30 +0400

    Changed Label Font Sizes

--- src/components/components/Label.jsx
+++ src/components/components/Label.jsx
@@ -1,6 +1,6 @@
 const Label = ({ label }) => {
     return (
-        <div className='bg-shopee-red text-white px-[.50rem] py-[.10rem] rounded-[.30rem] self-start'>
+        <div className='bg-shopee-red text-[.625rem] md:text-[.75rem] text-white px-[.50rem] py-[.10rem] rounded-[.30rem] self-start'>
             {label}
         </div>
     )
